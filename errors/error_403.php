ee839bd2be7270ab107577edb1467125ffd2768b
Chris Pilkington <chris.pilkington@gmail.com>
2011-07-24 14:52:35 +1000

    Updated error messages.

---
+++ errors/error_403.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>
+			e r r o r ~ 403</title>
+    <link rel="stylesheet" type="text/css" href="style_b.css">
+  </head>
+    
+  <body bgcolor="#000000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" text="#ffffff">
+    <center>
+      <table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%">
+        <tr>
+					<td align="center" valign="center">
+            <a href="/"><img src="/errors/images/error_403.jpg" border="0"></a><br><br>
+            errors by <a href="http://www.guermant.com/">DiGi</a>
+        	</td>
+				</tr>
+      </table>
+    </center>
+  </body>
+</html>

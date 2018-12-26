0ad6d1568b2c7b413efa5a6bfb8940b458ff4ba3
Chris Pilkington <chris.pilkington@gmail.com>
2018-12-27 00:13:50 +1100

    Updated to PHP 7 mysql functions.

--- util/database.php
+++ util/database.php
@@ -11,21 +11,21 @@
       $password = "mypassword";
       $database = "mydatabase";
 
-      $this->db = mysql_connect($server,$username,$password);
-      @mysql_select_db($database) or die($this->Error("Unable to select database"));
+      $this->db = mysqli_connect($server,$username,$password);
+      @mysqli_select_db($this->db, $database) or die($this->Error("Unable to select database $database"));
       $this->open = true;
     }
 
     function Query($query)
     {
-      if ($this->open && $query) return mysql_query($query, $this->db);
+      if ($this->open && $query) return mysqli_query($this->db, $query);
 
       return 0;
     }
 
     function GetLastInsertID()
     {
-      return mysql_insert_id();
+      return mysqli_insert_id($this->db);
     }
 
     function Select($table, $selection = "", $where = "", $options = "")
@@ -57,7 +57,7 @@
     function GetRows($result)
     {
       if ($result) {
-        $numrows = mysql_numrows($result) or $numrows = 0;
+        $numrows = mysqli_num_rows($result) or $numrows = 0;
         return $numrows;
       }
 
@@ -67,14 +67,16 @@
 
     function GetFields($table)
     {
-      if ($table) return $this->query("SHOW COLUMNS from $table");
+      if ($table) return $this->Query("SHOW COLUMNS from $table");
 
       return null;
     }
 
     function CloseDB()
     {
-      if ($this->open) mysql_close();
+      if ($this->open) {
+        // TODO: Close the connection here?  Set it to null?
+      }
 
       $this->open = false;
     }
@@ -82,12 +84,12 @@
     function Information($errstr = "")
     {
       print "cDB->Information($errstr);<br>\n";
-      mysql_error();
+      mysqli_error($this->db);
     }
 
     function Error($errstr = "")
     {
-      echo "cDB->Error(" . $errstr . ");<br>\nmysql_error()=\"" . mysql_error() . "\"";
+      echo "cDB->Error(" . $errstr . ");<br>\nmysql_error()=\"" . mysqli_error($this->db) . "\"";
     }
   };
 ?>
